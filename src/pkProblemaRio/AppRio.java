baeb1aff7b714d75a868bd036e1b047d0186c72f
justinP3 <justin.pincay@epn.edu.ec>
2025-11-14 22:26:41 -0500

    parte

--- src/pkProblemaRio/AppRio.java
+++ src/pkProblemaRio/AppRio.java
@@ -1,10 +1,9 @@
 package pkProblemaRio;
 
-
 public class AppRio {
 
     public void iniciarJuegoRio() {
-        
+
         System.out.println("==================== BIENVENIDO AL JUEGO DE DECISIÓN ======================");
         System.out.println("Tienes una barca y 3 personajes que llevar, pero tienes un problema");
         System.out.println("Tu barca solo te aguanta a ti y alguien más que elijas");
